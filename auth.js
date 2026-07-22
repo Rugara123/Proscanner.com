683abb5a626a9881896cac772b7e1bf6b6231149
Rugara123 <144539458+Rugara123@users.noreply.github.com>
2026-07-22 23:50:08 +0300

    Add authentication constants and logging

--- auth.js
+++ auth.js
@@ -1,1 +1,5 @@
+const DERIV_CLIENT_ID = "33U5nPL3pG28rV3x8Q0fV";
+const REDIRECT_URI = "https://rugara123.github.io/Proscanner.com/";
 
+console.log("Proscanner Authentication Loaded");
+console.log("Client ID:", DERIV_CLIENT_ID);
