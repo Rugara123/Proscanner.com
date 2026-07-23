e4cc7f29ad0c454dba084d3c878ca8c85035cbdb
Rugara123 <144539458+Rugara123@users.noreply.github.com>
2026-07-23 07:42:45 +0300

    Integrate Firebase for authentication
    Updated authentication setup to use Firebase SDK for user authentication.

--- auth.js
+++ auth.js
@@ -1,5 +1,24 @@
-const DERIV_CLIENT_ID = "33U5nPL3pG28rV3x8Q0fV";
-const REDIRECT_URI = "https://rugara123.github.io/Proscanner.com/";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-console.log("Proscanner Authentication Loaded");
-console.log("Client ID:", DERIV_CLIENT_ID);
+const firebaseConfig = {
+  apiKey: "AIzaSyAEZnQRhK5nPoQaZgFX0moKd369Gd0AfAk",
+  authDomain: "proscanner-f2b15.firebaseapp.com",
+  projectId: "proscanner-f2b15",
+  storageBucket: "proscanner-f2b15.firebasestorage.app",
+  messagingSenderId: "997903347044",
+  appId: "1:997903347044:web:79f3305d71d87bbe65de71"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.firebaseAuth = auth;
+window.signInWithEmailAndPassword = signInWithEmailAndPassword;
+window.GoogleAuthProvider = GoogleAuthProvider;
+window.signInWithPopup = signInWithPopup;
